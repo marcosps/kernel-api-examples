64e1401af31d0618d30fcf1abefc899e348c4d3e
Marcos Paulo de Souza <mpdesouza@suse.com>
2024-10-27 16:13:45 -0300

    sconsole: Implement a simple miscdevice to read the console messages
    TODO: Implement a ringbuffer like struct to rotate the messages

--- sconsole.c
+++ sconsole.c
@@ -1,24 +1,89 @@
 /* Simple console implementation for experimenting legacy and NBCON variants */
 #define pr_fmt(fmt) KBUILD_MODNAME ": " fmt
 
-#include <linux/module.h>
 #include <linux/console.h>
+#include <linux/miscdevice.h>
+#include <linux/module.h>
+
+#define MAX_RECORDS 100
+
+struct scon_msg {
+	size_t len;
+	char msg[MAX_RECORDS];
+};
+
+static struct scon_msg msgs[MAX_RECORDS];
+static size_t nrecs;
+
+static ssize_t scon_read(struct file *f, char __user *buf, size_t count,
+		loff_t *fpos)
+{
+	/* Each fpos means a record here */
+	if (*fpos >= MAX_RECORDS || *fpos >= nrecs)
+		return 0;
+
+	/* only read the first message */
+	if (count > msgs[0].len)
+		count = msgs[0].len;
+
+	if (copy_to_user(buf, msgs[*fpos].msg, count))
+		return -ERESTARTSYS;
+
+	(*fpos)++;
+
+	return count;
+}
+
+static void write_msg(struct console *con, const char *msg, unsigned int len)
+{
+	/* For now store only MAX_RECORDS console entries */
+	if (nrecs >= MAX_RECORDS)
+		return;
+
+	msgs[nrecs].len = len;
+	strscpy(msgs[nrecs].msg, msg, sizeof(msgs[nrecs].msg));
+	nrecs++;
+}
+
+static struct file_operations scon_fops = {
+	.read = scon_read,
+};
+
+static struct miscdevice scon_misc = {
+	.minor = MISC_DYNAMIC_MINOR,
+	.name = "sconsole",
+	.mode = S_IRUGO,
+	.fops = &scon_fops,
+};
 
 static struct console scon = {
-	.name = "sconsole",
+	.name = "simple_console",
 	.flags = CON_ENABLED,
+	.write = write_msg,
 };
 
 static int sconsole_init(void)
 {
-	//pr_info("Registering simple console (sconsole)\n");
+	int ret;
+
+	/*
+	 * Register the miscdevice that will be responsible for reading the
+	 * console messages.
+	 */
+	ret = misc_register(&scon_misc);
+	if (ret)
+		return ret;
+
+	/* Registers simple console so kernel can start writing messages to it. */
 	register_console(&scon);
+
 	return 0;
 }
 
 static void sconsole_exit(void)
 {
 	unregister_console(&scon);
+	misc_deregister(&scon_misc);
 }
 
 module_init(sconsole_init);
